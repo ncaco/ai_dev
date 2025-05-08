629de5de87b4ee5697ff7f42391ca547fa67f60b
ncaco <ncaco97@gmail.com>
2025-05-08 19:43:22 +0900

    study

---
+++ 20250508/test_2.py
@@ -0,0 +1,52 @@
+
+p_hat = [
+    0.33,
+    0.66
+]
+
+y = [
+    0,
+    1
+]
+
+y_reverse = [
+    1,
+    0
+]
+
+for idx, (ph, yt) in enumerate(zip(p_hat, y), 1):
+    error = ph - yt
+    squared = ph ** 2
+    squared_error = squared - yt
+    print(f"[{idx}] 예측값: {ph:.2f}, 실제값: {yt}, 오차: {error:.2f}, 제곱: {squared:.4f}, (제곱-실제값): {squared_error:.4f}")
+
+
+
+    # 모든 (제곱 - 실제값) 값을 합산하여 출력
+    # squared_error 값을 리스트에 저장
+    if idx == 1:
+        squared_error_list = []
+    squared_error_list.append(squared_error)
+
+    if idx == len(p_hat):
+        total = sum(squared_error_list)
+        print(f"제곱오차를 뺀 값의 합: {total:.4f}")
+
+
+for idx, (ph, yt) in enumerate(zip(p_hat, y_reverse), 1):
+    error = ph - yt
+    squared = ph ** 2
+    squared_error = squared - yt
+    print(f"[{idx}] 예측값: {ph:.2f}, 실제값: {yt}, 오차: {error:.2f}, 제곱: {squared:.4f}, (제곱-실제값): {squared_error:.4f}")
+
+
+
+    # 모든 (제곱 - 실제값) 값을 합산하여 출력
+    # squared_error 값을 리스트에 저장
+    if idx == 1:
+        squared_error_list = []
+    squared_error_list.append(squared_error)
+
+    if idx == len(p_hat):
+        total = sum(squared_error_list)
+        print(f"제곱오차를 뺀 값의 합: {total:.4f}")
